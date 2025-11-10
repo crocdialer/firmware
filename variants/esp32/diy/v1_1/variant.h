696ba0cd606780910023399b93a30477bb889f69
crocdialer <crocdialer@gmail.com>
2025-11-10 11:32:26 +0100

    esp32 wroom custom wiring squeezed into diy-variant

--- variants/esp32/diy/v1_1/variant.h
+++ variants/esp32/diy/v1_1/variant.h
@@ -1,14 +1,39 @@
-// For OLED LCD
-#define I2C_SDA 21
+// display/sensors
 #define I2C_SCL 22
+#define I2C_SDA 23
 
-// GPS
+// no GPS
 #undef GPS_RX_PIN
-#define GPS_RX_PIN 15
+// #define GPS_RX_PIN 15
 
-#define BUTTON_PIN 2 // The middle button GPIO on the T-Beam
+// A battery voltage measurement pin, voltage divider connected here to measure battery voltage
+#define BATTERY_PIN 35
+#define ADC_CHANNEL ADC1_GPIO35_CHANNEL
+#define ADC_MULTIPLIER 2.0
+
+// custom button
+#define BUTTON_PIN 14
 #define BUTTON_NEED_PULLUP
-#define EXT_NOTIFY_OUT 12 // Overridden default pin to use for Ext Notify Module (#975).
+
+// red onboard LED
+#define PIN_LED1 (13)
+
+// wired button LED
+#define PIN_LED2 (12)
+#define ledOff(pin) digitalWrite(pin, LOW);//pinMode(pin, INPUT)
+
+// disable shitty default LED, it's annoying and has little meaning
+#define LED_PIN PIN_LED1
+
+// board LED -> bluetooth connection
+#define BLE_LED PIN_LED2
+
+// button-LED -> new message // TODO: requires some changes to also work with custom esp32 
+// #define LED_CONN PIN_LED2
+
+#define LED_STATE_ON 1 // State when LED is litted
+
+// #define EXT_NOTIFY_OUT PIN_LED2 // Overridden default pin to use for Ext Notify Module (#975).
 
 #define LORA_DIO0 26  // a No connect on the SX1262/SX1268 module
 #define LORA_RESET 27 // RST for SX1276, and for SX1262/SX1268
@@ -16,33 +41,20 @@
 #define LORA_DIO2 32  // BUSY for SX1262/SX1268
 #define LORA_DIO3     // Not connected on PCB, but internally on the TTGO SX1262/SX1268, if DIO3 is high the TXCO is enabled
 
-// In transmitting, set TXEN as high communication level，RXEN pin is low level;
-// In receiving, set RXEN as high communication level, TXEN is lowlevel;
-// Before powering off, set TXEN、RXEN as low level.
-#define LORA_RXEN 14 // Input - RF switch RX control, connecting external MCU IO, valid in high level
-#define LORA_TXEN 13 // Input - RF switch TX control, connecting external MCU IO or DIO2, valid in high level
-
 #undef LORA_SCK
-#define LORA_SCK 18
+#define LORA_SCK 5
 #undef LORA_MISO
 #define LORA_MISO 19
 #undef LORA_MOSI
-#define LORA_MOSI 23
+#define LORA_MOSI 18
 #undef LORA_CS
-#define LORA_CS 5
-
-// RX/TX for RFM95/SX127x
-#define RF95_RXEN LORA_RXEN
-#define RF95_TXEN LORA_TXEN
-// #define RF95_TCXO <GPIO#>
+#define LORA_CS 4
 
 // common pinouts for SX126X modules
-#define SX126X_CS 5
+#define SX126X_CS LORA_CS
 #define SX126X_DIO1 LORA_DIO1
 #define SX126X_BUSY LORA_DIO2
 #define SX126X_RESET LORA_RESET
-#define SX126X_RXEN LORA_RXEN
-#define SX126X_TXEN LORA_TXEN
 
 // supported modules list
 #define USE_RF95 // RFM95/SX127x
@@ -50,9 +62,12 @@
 #define USE_SX1268
 #define USE_LLCC68
 
-#ifdef EBYTE_E22
-// Internally the TTGO module hooks the SX126x-DIO2 in to control the TX/RX switch
-// (which is the default for the sx1262interface code)
-#define SX126X_DIO3_TCXO_VOLTAGE 1.8
-#define TCXO_OPTIONAL // make it so that the firmware can try both TCXO and XTAL
-#endif
+// static inline void initVariant()
+// {
+//     // LED1 & LED2
+//     pinMode(PIN_LED1, OUTPUT);
+//     ledOff(PIN_LED1);
+
+//     pinMode(PIN_LED2, OUTPUT);
+//     ledOff(PIN_LED2);
+// }
